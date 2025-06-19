d3fa4166fe66701065c021b103d76c7467b7efcc
dhaneshwar <dhaneshwar6206@gmail.com>
2025-06-19 18:53:09 +0530

    responsive

--- src/component/Display.tsx
+++ src/component/Display.tsx
@@ -5,10 +5,10 @@ import { signIn } from "next-auth/react";
 const Display = () => {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center'>
-      <div className='text-6xl font-bold'>
+      <div className='sm:text-6xl text-5xl font-bold'>
       AI-<span className='text-primary'>CHATBOT</span>
       </div>
-      <div className='text-xl text-muted-foreground'>
+      <div className='sm:text-xl text-muted-foreground'>
         Powered by Gemini API
       </div>
       <div className='mt-4 text-xl' onClick={() => signIn()}>
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Button from './Button'
 import History from '../icons/History'
-import Menu from '../icons/Menu'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Navbar = () => {
@@ -12,7 +11,7 @@ const Navbar = () => {
       <ul className='w-full sm:w-auto flex sm:gap-4 justify-around items-center'>
         <li className='text-muted-foreground'><History /></li>
         <li className='font-bold'>AI-<span className='text-primary'>CHATBOT</span></li>
-        <li className='sm:hidden'><Menu /></li>
+        <li className='sm:hidden text-foreground' onClick={() => signOut()}><Button text={"Signout"} type='active'/></li>
       </ul>
       <ul className='hidden sm:flex gap-8 items-center text-muted-foreground'>
         <li className='text-foreground'>Home</li>
